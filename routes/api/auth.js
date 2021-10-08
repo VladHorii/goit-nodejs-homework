ee585e4f4b61a908ad13de7fbc813aaf64dc7bb2
Vlad Horii <VladHorii@yandex.ru>
2021-10-09 00:46:28 +0300

    added new route

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -13,5 +13,6 @@ router.post("/signup", validation(joiSchema), controllerWrapper(ctrl.signup));
 router.post("/login", validation(joiSchema), controllerWrapper(ctrl.login));
 router.get("/logout", authenticate, controllerWrapper(ctrl.logout));
 router.get("/current", authenticate, controllerWrapper(ctrl.current));
+router.patch("/", authenticate, controllerWrapper(ctrl.updateSubscription));
 
 module.exports = router;
