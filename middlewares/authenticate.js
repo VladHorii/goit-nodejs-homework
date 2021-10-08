49bc381c6cd41e7b8e7dc32f5ed8220f982899eb
Vlad Horii <VladHorii@yandex.ru>
2021-10-08 23:04:51 +0300

    added new middlewares

---
+++ middlewares/authenticate.js
@@ -0,0 +1,34 @@
+const jwt = require("jsonwebtoken");
+const { User } = require("../models");
+const { SECRET_KEY } = process.env;
+
+const authenticate = async (req, res, next) => {
+  try {
+    const { authorization } = req.headers;
+    if (!authorization) {
+      throw new Error("Not authorized");
+    }
+
+    const [bearer, token] = authorization.split(" ");
+    if (bearer !== "Bearer") {
+      throw new Error("Not authorized");
+    }
+
+    const { _id } = jwt.verify(token, SECRET_KEY);
+    const user = await User.findById(_id);
+    if (!user.token) {
+      throw new Error("Not authorized");
+    }
+    req.user = user;
+    next();
+  } catch (error) {
+    res.status(401).json({
+      status: "error",
+      code: 401,
+      message: "Not authorized",
+    });
+    return;
+  }
+};
+
+module.exports = authenticate;
