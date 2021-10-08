654d806a350f02b4c93ef1d9f2163537386e8ce9
Vlad Horii <VladHorii@yandex.ru>
2021-10-09 00:19:19 +0300

    add filter by field

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -1,14 +1,10 @@
 const { Contact } = require("../../models");
 
-const getAll = async (req, res, next) => {
-  try {
-    const contactslist = await Contact.find(
-      {},
-      "_id name email phone favorite"
-    );
-    res.json(contactslist);
-  } catch (error) {
-    next(error);
-  }
+const getAll = async (req, res) => {
+  const contactslist = await Contact.find(
+    req.query,
+    "_id name email phone favorite owner"
+  );
+  res.json(contactslist);
 };
 module.exports = getAll;
