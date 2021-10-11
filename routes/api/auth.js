50b0b77ea4ff0bbc6a2aaf83bf00d8bd7594eae7
Vlad Horii <VladHorii@yandex.ru>
2021-10-11 13:47:02 +0300

    added new route to update the user`s avatar

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -5,6 +5,7 @@ const {
   controllerWrapper,
   validation,
   authenticate,
+  upload,
 } = require("../../middlewares");
 
 const router = express.Router();
@@ -14,5 +15,11 @@ router.post("/login", validation(joiSchema), controllerWrapper(ctrl.login));
 router.get("/logout", authenticate, controllerWrapper(ctrl.logout));
 router.get("/current", authenticate, controllerWrapper(ctrl.current));
 router.patch("/", authenticate, controllerWrapper(ctrl.updateSubscription));
+router.patch(
+  "/avatars",
+  authenticate,
+  upload.single("photo"),
+  controllerWrapper(ctrl.updateAvatar)
+);
 
 module.exports = router;
