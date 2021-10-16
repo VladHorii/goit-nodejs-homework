c242071489904ccf4dc8926425c3eb9204330240
Vlad Horii <VladHorii@yandex.ru>
2021-10-16 22:53:54 +0300

    send email after register

--- controllers/auth/signup.js
+++ controllers/auth/signup.js
@@ -1,6 +1,8 @@
 const { Conflict } = require("http-errors");
 const { User } = require("../../models");
 const gravatar = require("gravatar");
+const { nanoid } = require("nanoid");
+const { sendEmail } = require("../../helpers");
 
 const signup = async (req, res) => {
   const { email, password } = req.body;
@@ -13,9 +15,20 @@ const signup = async (req, res) => {
     { s: "100", r: "x", d: "retro" },
     false
   );
-  const newUser = new User({ email, avatarURL });
+  const verifyToken = nanoid(6);
+
+  const newUser = new User({ email, avatarURL, verifyToken });
   newUser.setPassword(password);
   await newUser.save();
+
+  const data = {
+    to: email,
+    subject: "Подтверждение регистрации на сайте",
+    html: `<a href="http://localhost:3000/api/users/verify/${verifyToken}" target="_blank">Для подтрвеждения почты перейдите по ссылке</a>`,
+  };
+
+  await sendEmail(data);
+
   res.status(201).json({
     status: "201 Created",
     code: 201,
